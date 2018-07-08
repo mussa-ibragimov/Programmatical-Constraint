d60450e64fc8063908b50f22509e8fb86c32956a
Mussa Ibragimov <ibragimov@kolesa.kz>
2018-07-08 18:50:35 +0600

    Added new method constrain set method example: Circle

---
+++ app/src/main/java/mussa/ibragimov/programmaticalconstraint/CircleActivity.kt
@@ -0,0 +1,12 @@
+package mussa.ibragimov.programmaticalconstraint
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CircleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_circle)
+    }
+}
